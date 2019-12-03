5d7939a8880798ba721baa3c2478f56ad005bc42
ishpartko <ishpartko@gmail.com>
2019-12-04 00:35:55 +0300

    add store module

--- src/store/index.js
+++ src/store/index.js
@@ -5,10 +5,17 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    mapTree: null,
   },
   mutations: {
+    mapTree(state, value) {
+      state.mapTree = value
+    }
   },
   actions: {
+    requestMapTree(state) {
+      state.commit()
+    }
   },
   modules: {
   }
