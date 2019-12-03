eca87bb8d4d06bf3fea31f3d00f953bfcd90d267
ishpartko <ishpartko@gmail.com>
2019-12-04 00:35:07 +0300

    feat: update layout and logic of app

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,65 @@
 <template>
   <div class="app">
-    <MapTree></MapTree>
-    <Map></Map>
-    <SavedMapTree></SavedMapTree>
+    <CreateMapTree></CreateMapTree>
+    <template v-if="false">
+      <MapTree class="app-map-tree"></MapTree>
+      <Map class="app-map"></Map>
+      <SavedMapTree class="saved-map-tree"></SavedMapTree>
+    </template>
   </div>
 </template>
 
 <script>
-import Map from './components/Map.vue'
-import MapTree from './components/MapTree.vue'
-import SavedMapTree from './components/SavedMapTree.vue'
+import CreateMapTree from "./components/CreateMapTree.vue";
+import Map from "./components/Map.vue";
+import MapTree from "./components/MapTree.vue";
+import SavedMapTree from "./components/SavedMapTree.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
     Map,
     MapTree,
-    SavedMapTree
+    SavedMapTree,
+    CreateMapTree
   }
-}
+};
 </script>
 
-<style>
+<style scoped>
 .app {
   display: flex;
-  justify-content: space-between;
+  flex-direction: column;
+}
+
+.app-map {
+  height: 70vh;
+}
+
+@media (min-width: 700px) {
+  :root {
+    --tree-width: 400px;
+  }
+
+  .app {
+    display: flex;
+    justify-content: space-between;
+    height: 100vh;
+    flex-direction: row;
+  }
+
+  .app-map-tree {
+    flex-grow: 1;
+    flex-basis: var(--tree-width);
+  }
+
+  .app-map {
+    flex-grow: 2;
+  }
+
+  .saved-map-tree {
+    flex-grow: 1;
+    flex-basis: var(--tree-width);
+  }
 }
 </style>
