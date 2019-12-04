e64093e343fe69f33d5ab525ec52d4a0a51f8777
ishpartko <ishpartko@gmail.com>
2019-12-04 13:07:02 +0300

    update store methods

--- src/store/index.js
+++ src/store/index.js
@@ -6,10 +6,22 @@ Vue.use(Vuex)
 export default new Vuex.Store({
   state: {
     mapTree: null,
+    savedMapTree: null,
   },
   mutations: {
     mapTree(state, value) {
       state.mapTree = value
+    },
+    savedMapTree(state, value) {
+      state.savedMapTree = value
+    }
+  },
+  getters: {
+    mapTree(state) {
+      return state.mapTree
+    },
+    savedMapTree(state) {
+      return state.savedMapTree
     }
   },
   actions: {
