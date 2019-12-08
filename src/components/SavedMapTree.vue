ae9fd64a1ffd34b53fc187d35a07020d63557c84
ishpartko <ishpartko@gmail.com>
2019-12-08 15:34:58 +0300

    add saving map markers component

--- src/components/SavedMapTree.vue
+++ src/components/SavedMapTree.vue
@@ -1,15 +1,39 @@
 <template>
-  <div>
-    Сохраненное дерево
-  </div>
+  <ul>
+    <li 
+      v-for="place in savedPlaces" 
+      :key="place.id"
+      class="saved-list-item"
+      @click="onClickDelete(place)"
+    >
+      {{`${place.context.countryName} => ${place.context.regionName} => ${place.context.cityName}`}}
+    </li>
+  </ul>
 </template>
 
 <script>
+import { mapGetters } from "vuex";
 export default {
-
+  computed: {
+    ...mapGetters({
+      savedPlaces: "savedPlaces"
+    }),
+  },
+  methods: {
+    onClickDelete(place) {
+      this.$store.commit('unSaveMapMarker', place)
+    }
+  }
 }
 </script>
 
 <style>
-
+  .saved-list-item {
+    padding: 5px 10px;
+    border: 1px solid #000;
+    cursor: pointer;
+  }
+  .saved-list-item:hover {
+    background-color: rgba(255, 0, 0, 0.664)
+  }
 </style>
