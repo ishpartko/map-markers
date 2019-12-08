d13a43ed4d239dddf7c4143c134527041bc16816
ishpartko <ishpartko@gmail.com>
2019-12-08 15:36:49 +0300

    add place heplers

---
+++ src/helpers/place.js
@@ -0,0 +1,25 @@
+import {get} from 'lodash-es'
+const maxDeepSize = 3
+
+export const getPlaceContextFromFullApiContext = (fullApiContext) => {
+  const apiContext = fullApiContext
+    .reverse()
+    .slice(0, maxDeepSize)
+  return {
+  countryName: get(apiContext, '[0].text', 'Нет Страны'),
+  regionName: get(apiContext, '[1].text', 'Нет Области'),
+  cityName: get(apiContext, '[2].text', 'Нет Города')
+  }
+}
+
+export const getPlace = ({id, position}, fullApiContext) => {
+  const context = getPlaceContextFromFullApiContext(fullApiContext)
+  return {
+    id,
+    title: context.cityName,
+    position,
+    isSaved: false,
+    isShowOnMap: false,
+    context
+  }
+}
