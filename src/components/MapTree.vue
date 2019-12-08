c9d160347c143e0d7bf1cf57770cb4319bb2d76a
ishpartko <ishpartko@gmail.com>
2019-12-08 15:34:38 +0300

    update creating and display tree

--- src/components/MapTree.vue
+++ src/components/MapTree.vue
@@ -1,16 +1,88 @@
 <template>
-  <div>
+  <div class="map-tree-wrapper">
     <slot></slot>
-    Дерево
+    <ul v-if="tree" class="map-tree">
+      <TreeListItem
+        v-for="(place, key) in tree"
+        :key="key"
+        :place="place"
+        @selected="onSelected"
+      ></TreeListItem>
+    </ul>
   </div>
 </template>
 
 <script>
+import { mapGetters } from "vuex";
+import TreeListItem from "./TreeListItem.vue";
+import { merge } from "lodash-es";
 export default {
-
-}
+  components: {
+    TreeListItem
+  },
+  computed: {
+    ...mapGetters({
+      places: "places"
+    }),
+    tree() {
+      return this.createTreeFromPlaces(this.places);
+    }
+  },
+  methods: {
+    onSelected(treeItem) {
+      this.$store.commit("addMapMarkers", treeItem);
+    },
+    getHashCode(str) {
+      var hash = 0,
+        i,
+        chr;
+      if (str.length === 0) return hash;
+      for (i = 0; i < str.length; i++) {
+        chr = str.charCodeAt(i);
+        hash = (hash << 5) - hash + chr;
+        hash |= 0;
+      }
+      return hash;
+    },
+    createTreeItem(place) {
+      return {
+        [this.getHashCode(place.context.countryName)]: {
+          title: place.context.countryName,
+          children: {
+            [this.getHashCode(place.context.regionName)]: {
+              title: place.context.regionName,
+              children: {
+                [this.getHashCode(place.context.cityName)]: place
+              }
+            }
+          }
+        }
+      };
+    },
+    createTreeFromPlaces(places) {
+      return Object.values(places).reduce((result, place) => {
+        const treeItem = this.createTreeItem({...place});
+        return merge(treeItem, result);
+      }, {});
+    }
+  }
+};
 </script>
 
 <style>
+.map-tree-wrapper {
+  max-height: 100vh;
+  display: flex;
+  flex-direction: column;
+}
 
+.map-tree {
+  max-height: 100%;
+  overflow-y: scroll;
+  overflow-x: hidden;
+}
+
+.map-tree ul {
+  list-style-type: none;
+}
 </style>
