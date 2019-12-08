e1dc42bb21aa4ed0cf7415a374c0aea76f9b573d
ishpartko <ishpartko@gmail.com>
2019-12-08 15:37:24 +0300

    add Place class

---
+++ src/models/Place.js
@@ -0,0 +1,15 @@
+export class Place {
+  static isValid(value) {
+    let isValid = true
+    if(this.hasChildren(value)) {
+      isValid = this.isChildrenValid(value.children)
+    }
+    return value.hasOwnProperty('title') && isValid
+  }
+  static hasChildren(place) {
+    return place.hasOwnProperty('children')
+  }
+  static isChildrenValid(children) {
+    return typeof(children) === 'object'
+  }
+}
