2cdfa75e6b936fd057fe4726b6050460b42b778f
ishpartko <ishpartko@gmail.com>
2019-12-08 15:34:15 +0300

    update map logic

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -5,6 +5,8 @@
 </template>
 
 <script>
+import {mapGetters} from 'vuex'
+import Vue from 'vue'
 import {
   tileLayer, 
   map,
@@ -12,42 +14,80 @@ import {
 } from '@/lib/leaflet'
 import { mapToken } from '@/config'
 import 'leaflet/dist/leaflet.css'
-
-const defaults = {
-  position: [51.505, -0.09],
-  zoom: 13,
-  id: 'mapbox/streets-v11'
-} 
+import {has} from 'lodash-es'
+import {defaultView} from '@/helpers/map'
+import { getPlaceContextFromFullApiContext } from '@/helpers/place'
 
 export default {
   data() {
     return {
-      mainMap: null
+      mainMap: null,
+      markerIdsOnMap: {}
+    }
+  },
+  computed: {
+    ...mapGetters({
+      mapMarkers: 'mapMarkers',
+      places: 'places'
+    })
+  },
+  watch: {
+    mapMarkers(newValue) {
+      if(newValue === null) return;
+      newValue.forEach(place => {
+        this.addMarker(place)
+      });
     }
   },
   mounted() {
-    
     this.mainMap = map(this.$refs.map, {
-      center: defaults.position,
-      zoom: defaults.zoom
+      center: defaultView.position,
+      zoom: defaultView.zoom
     });
-    
+
+    this.mainMap.on('contextmenu', ()=> {})
+
     tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
       attribution: '',
       maxZoom: 18,
-      id: defaults.id,
+      id: defaultView.id,
       accessToken: mapToken
     }).addTo(this.mainMap);
-
-    this.mainMap.on('click', defaults.id, (e) => {
-      // eslint-disable-next-line no-console
-      console.log(e);
-    });
-
-    marker(defaults.position).addTo(this.mainMap).on('mouseover', (e) => {
-      // eslint-disable-next-line no-console
-      console.log(e.latlng);
-    });
+  },
+  methods: {
+    addMarker(place) {
+      if(has(this.markerIdsOnMap, place.id)) return;
+      const markerConfig = {
+        draggable:'true',
+        id: place.id
+      }
+      this.markerIdsOnMap[place.id] = marker(place.position, markerConfig)
+      this.markerIdsOnMap[place.id].on('click', () => {
+        this.$store.commit('saveMapMarker', place)
+        this.mainMap.removeLayer(this.markerIdsOnMap[place.id]);
+        Vue.delete(this.markerIdsOnMap, place.id)
+      });
+      this.markerIdsOnMap[place.id].on('dragend', (event)=> {
+        const marker = event.target;
+        const position = marker.getLatLng();
+        marker.setLatLng(position, markerConfig)
+        marker.bindPopup(position)
+        marker.update();
+        const updatedPlaceSync = {
+          ...this.places[place.id],
+          position
+        }
+        this.$store.dispatch('getPlaceContextFromApi', updatedPlaceSync).then((fullContext)=> {
+          const context = getPlaceContextFromFullApiContext(fullContext)
+          const updatedPlaceAsync = {
+            ...updatedPlaceSync,
+            context
+          }
+          this.$store.commit('updateSinglePlace', updatedPlaceAsync)
+        })
+      })
+      this.markerIdsOnMap[place.id].addTo(this.mainMap)
+    }
   }
 }
 </script>
